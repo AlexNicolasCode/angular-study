c1a133577b7e07f6859c917a815805f9f9d31fb8
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-20 17:58:11 -0300

    feat: Home page was created

---
+++ src/presentation/pages/home/script.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.html',
+})
+
+export class HomePage {}

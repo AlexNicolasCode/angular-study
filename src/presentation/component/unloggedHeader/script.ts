c425b47d4a698b8dd9b1734e4605ac6ee99eb627
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-20 17:57:51 -0300

    feat: UnloggedHeader component was created

---
+++ src/presentation/component/unloggedHeader/script.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './unlogged.header.html',
+})
+
+export class HeaderComponent {}

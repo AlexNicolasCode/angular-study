c21b58931bf3ed2e983e6baf24f446fe09406c5d
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-21 13:49:26 -0300

    feat: undefined files should have types as undefined to avoid conflit types

--- src/domain/usecase/login.with.google.ts
+++ src/domain/usecase/login.with.google.ts
@@ -4,9 +4,9 @@ export interface LoginWithGoogle {
 
 export namespace LoginWithGoogle {
     export type Result = {
-        name: string
-        email: string
-        photoUrl: string
+        name?: string
+        email?: string
+        photoUrl?: string
         isEmailVerified: boolean
     }
 }

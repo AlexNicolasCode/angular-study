7a5ba3a98c281643edf5d74439689883307a4a34
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-21 10:37:22 -0300

    feat: user should be redirected to login screen when try access home page

--- src/presentation/route/index.ts
+++ src/presentation/route/index.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: "",
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: "login",
         loadComponent: () => import("../pages/home").then((m) => m.HomePage)
     }
 ];

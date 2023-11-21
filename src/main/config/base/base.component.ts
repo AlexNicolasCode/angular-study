65a8edee5f462fb9a93a19127c97b08aa6770411
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-21 11:59:24 -0300

    fix: update import paths to enable development build project

--- src/main/config/base/base.component.ts
+++ src/main/config/base/base.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from "@/presentation/component";
+import { HeaderComponent } from "../../../presentation/component";
 
 @Component({
     selector: 'root',
--- src/main/config/base/base.config.ts
+++ src/main/config/base/base.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 
-import { routes } from '@/presentation/route';
+import { routes } from '../../../presentation/route';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()]

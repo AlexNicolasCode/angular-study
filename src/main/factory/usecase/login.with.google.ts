2ad36d99a977ae4a3afd1f5cea495981f987e858
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-30 22:35:31 -0300

    fix: change path name to correct path to enable import correctly LocaStorageRepository

--- src/main/factory/usecase/login.with.google.ts
+++ src/main/factory/usecase/login.with.google.ts
@@ -1,7 +1,7 @@
 import { RemoteLogin } from "@/data/usecase";
 import { LoginWithGoogle } from "@/domain/usecase";
 import { FirebaseAuthentication } from "@/infra/auth";
-import { LocalStorageRepository } from "@/infra/db";
+import { LocalStorageRepository } from "@/infra/local-storage";
 
 export const makeLoginWithGoogle = (): LoginWithGoogle => {
     const firebaseAuthentication = new FirebaseAuthentication()
